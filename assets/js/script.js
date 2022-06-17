e9920a7a51f0a69f239b0c787aef777daf18515c
Britt Carlton <brittanyrc95@gmail.com>
2022-06-17 19:53:19 -0400

    Add time checking, description bg color change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 // variables
 // schedule element selector
 var scheduleEl = $("#schedule");
-// current time object
-var currentTime = moment();
 // schedule item descriptions
 var itemDescriptions = [
     "", "", "", "", "", "", "", "", "", "", "", "", ""
@@ -11,8 +9,74 @@ var itemDescriptions = [
 // functions
 // function to update textarea color based on current time
 function timeColorChange() {
+    // get the current time
+    var currentTime = checkTime();
 
+    // iterate through schedule el to check each row time agains current time
+    $.each(scheduleEl.children(), function(i, item) {
+        // get textarea element to change background
+        var textareaEl = $(item).children(".description");
+        // get row time
+        var rowTime = new String($(item).children(".hour").text());
+        
+        // check for schedule past
+        if (formatTime(rowTime) < currentTime) {
+            // remove present, future bg colors
+            $(textareaEl).removeClass("present", "future");
+            // add past bg color
+            $(textareaEl).addClass("past");
+        }
+        // check for schedule present
+        else if (formatTime(rowTime) == currentTime) {
+            // remove past, future bg colors
+            $(textareaEl).removeClass("past", "future");
+            // add present bg color
+            $(textareaEl).addClass("present");
+        }
+        // check for schedule future
+        else if (formatTime(rowTime) > currentTime) {
+            // remove past, present bg colors
+            $(textareaEl).removeClass("past", "present");
+            // add future bg color
+            $(textareaEl).addClass("future");
+        };
+    });
+    
 }
+
+// function to format row time as 24hr, no suffix
+function formatTime(time) {
+    // check for AM/PM, make 24hr time
+    if (time.endsWith("PM")) {
+        // remove PM
+        time = time.replace("PM", "").trim();
+        
+        // check for noon to skip 24hr format
+        if (time.startsWith("12")) {
+            // make int for time comparison
+            time = parseInt(time);
+        } else {
+            // make int for time comparison, change to 24hr format
+            time = parseInt(time) + 12;
+        }
+    } else if (time.endsWith("AM")) {
+        // remove AM, make int for time comparison
+        time = parseInt(time.replace("AM", "").trim());
+    }
+
+    // return formatted time
+    return time;
+}
+
+// function to automatically run timeColorChange() every minute
+function checkTime() {
+    // get current date and time
+    var currentDate = new Date();
+    
+    // return current hours
+    return currentDate.getHours();
+}
+
 
 // function to save schedule
 function saveSchedule(target) {
@@ -48,3 +112,4 @@ scheduleEl.on("click", ".saveBtn", function() {
 
 // on page load
 loadSchedule();
+timeColorChange();
